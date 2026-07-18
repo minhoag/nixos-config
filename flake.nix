65d0decf7c400181754cd0069a9c1661ea8e98e0
hoang.nguyen <nhoang120899@gmail.com>
2026-07-18 17:13:31 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "My NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
