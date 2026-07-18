5ac2bc3169f260ed4b798b83636048a24dc19860
hoang.nguyen <nhoang120899@gmail.com>
2026-07-18 23:53:30 +0700

    update config

--- modules/hardware/video/amdgpu.nix
+++ modules/hardware/video/amdgpu.nix
@@ -5,18 +5,11 @@
     enable = true;
     videoDrivers = [ "amdgpu" ];
   };
-  environment.systemPackages = with pkgs; [ rocmPackages.amdsmi ];
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [
-      amdvlk
-      libvdpau-va-gl
-      vaapiVdpau
-      # vulkan-loader
-      # vulkan-extension-layer
-      # vulkan-validation-layers
-    ];
-    extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
   };
+  environment.systemPackages = with pkgs; [
+    rocmPackages.amdsmi
+  ];
 }
