931894118622391ab8fe608f11c39a63bd8f40e1
hoang.nguyen <nhoang120899@gmail.com>
2026-07-18 22:36:11 +0700

    update config

---
+++ modules/core/security/default.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+{
+  security = {
+    rtkit.enable = true;
+    polkit.enable = true;
+    pam.services.sddm.enableGnomeKeyring = true;
+    sudo.extraConfig = "Defaults pwfeedback"; # Show asterisks when typing sudo password
+    apparmor = {
+      enable = true;
+      killUnconfinedConfinables = true;
+      packages = [ pkgs.apparmor-profiles ];
+    };
+  };
+}
