2be26dd32189c6ca37fcedfa044520f153137732
hoang.nguyen <nhoang120899@gmail.com>
2026-07-20 18:29:29 +0700

    feat: update bg image

--- modules/core/sddm/default.nix
+++ modules/core/sddm/default.nix
@@ -10,22 +10,22 @@ let
     version = "unstable";
     src = inputs.samaritan-sddm-theme;
     installPhase = ''
-      mkdir -p $out/share/sddm/themes/samaritan
-      cp -r . $out/share/sddm/themes/samaritan
-      cp ${../../wallpapers/gyarados.jpg} $out/share/sddm/themes/samaritan/gyarados.jpg
-      substituteInPlace $out/share/sddm/themes/samaritan/Main.qml \
-        --replace-fail 'SAMARITAN v1.2.951.04' 'WUMPS' \
-        --replace-fail 'text: "SAMARITAN"' 'text: "WUMPS"' \
-        --replace-fail 'Rectangle {' 'Rectangle {
-    Image {
-        anchors.fill: parent
-        source: "gyarados.jpg"
-        fillMode: Image.PreserveAspectCrop
-    }'
-      substituteInPlace $out/share/sddm/themes/samaritan/components/PasswordField.qml \
-        --replace-fail 'border.color: colors.background' 'border.color: "transparent"' \
-        --replace-fail 'color: colors.background' 'color: "transparent"' \
-        --replace-fail 'color: colors.primaryText' 'color: "#2B2418"'
+        mkdir -p $out/share/sddm/themes/samaritan
+        cp -r . $out/share/sddm/themes/samaritan
+        cp ${./bg.jpg} $out/share/sddm/themes/samaritan/bg.jpg
+        substituteInPlace $out/share/sddm/themes/samaritan/Main.qml \
+          --replace-fail 'SAMARITAN v1.2.951.04' 'WUMPS' \
+          --replace-fail 'text: "SAMARITAN"' 'text: "WUMPS"' \
+          --replace-fail 'Rectangle {' 'Rectangle {
+      Image {
+          anchors.fill: parent
+          source: "bg.jpg"
+          fillMode: Image.PreserveAspectCrop
+      }'
+        substituteInPlace $out/share/sddm/themes/samaritan/components/PasswordField.qml \
+          --replace-fail 'border.color: colors.background' 'border.color: "transparent"' \
+          --replace-fail 'color: colors.background' 'color: "transparent"' \
+          --replace-fail 'color: colors.primaryText' 'color: "#2B2418"'
     '';
   };
 in
