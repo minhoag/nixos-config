eee73381feb89da3526ebb68c17c80287abd18d9
hoang.nguyen <nhoang120899@gmail.com>
2026-07-24 00:43:10 +0700

    feat: add virtualisation

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -6,6 +6,8 @@ let
     chmod -R u+w $out
     cp $out/scheme/default.lua $out/scheme/current.lua
     cp ${./caelestia/keybinds.lua} $out/hyprland/keybinds.lua
+    substituteInPlace $out/hyprland/execs.lua \
+      --replace-fail $'    -- Start shell\n    hl.exec_cmd("caelestia shell -d")\n' ""
   '';
 in
 {
@@ -25,28 +27,38 @@ in
 
   home-manager.sharedModules = [
     inputs.caelestia-shell.homeManagerModules.default
-    ({ config, lib, pkgs, ... }: {
-      home.packages = [ caelestiaWallpaperEngine ];
+    (
+      {
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+      {
+        home.packages = [ caelestiaWallpaperEngine ];
 
-      xdg.configFile = {
-        hypr.source = hyprConfig;
-        caelestia.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/desktop/hyprland/caelestia";
-      };
+        xdg.configFile = {
+          hypr.source = hyprConfig;
+          caelestia.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/desktop/hyprland/caelestia";
+        };
 
-      home.activation.removeNiriState = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-        rm -rf "$HOME/.config/niri" "$HOME/.config/DankMaterialShell"
-      '';
+        home.activation.removeNiriState = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+          rm -rf "$HOME/.config/niri" "$HOME/.config/DankMaterialShell"
+        '';
 
-      programs.caelestia = {
-        enable = true;
-        systemd.enable = false;
-        cli.enable = true;
-        package = inputs.caelestia-shell.packages.${pkgs.stdenv.hostPlatform.system}.with-cli.overrideAttrs (old: {
-          postPatch = (old.postPatch or "") + ''
-            cp -r ${./caelestia/quickshell}/. .
-          '';
-        });
-      };
-    })
+        programs.caelestia = {
+          enable = true;
+          systemd.enable = true;
+          cli.enable = true;
+          package =
+            inputs.caelestia-shell.packages.${pkgs.stdenv.hostPlatform.system}.with-cli.overrideAttrs
+              (old: {
+                postPatch = (old.postPatch or "") + ''
+                  cp -r ${./caelestia/quickshell}/. .
+                '';
+              });
+        };
+      }
+    )
   ];
 }
